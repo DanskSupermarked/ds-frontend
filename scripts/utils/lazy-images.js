5f184854302194e726065370dc3e1011c8848140
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2014-10-09 15:28:08 +0200

    Tests for utils/lazy-images

--- scripts/utils/lazy-images.js
+++ scripts/utils/lazy-images.js
@@ -1,71 +1,87 @@
 /**
  * Lazy load images with class 'lazy'.
  */
-
-// Dependencies
-var $ = require('jquery');
-
-// Percent under bottom of screen images is loaded. A value of 0 will load
-// images when they are within visible area of window.
-var OFFSET_PERCENT = 1;
-
-var queued = false;
-var $lazyImages;
-var $document = $(document);
-var $window = $(window);
-
-var loadImgSrc = function($img) {
-    if ($img.data('srcset')) {
-        $img.attr('srcset', $img.data('srcset'));
-    } else if ($img.data('srcset')) {
-        $img.attr('src', $img.data('src'));
+(function(root, factory) {
+    if (typeof exports === 'object') {
+        exports = factory(require('jquery'));
+    } else {
+        root.ds = root.ds || {};
+        root.ds.utils = root.ds.utils || {};
+        root.ds.utils.lazyImages = factory(root.jQuery);
     }
-};
+}(this, function($) {
+    var exports = {};
 
-var checkForLazyImages = function() {
+    // Percent under bottom of screen images is loaded. A value of 0 will load
+    // images when they are within visible area of window.
+    var OFFSET_PERCENT = 1;
 
-    // Avoid continuous scroll events
-    if (queued) {
-        return;
-    }
-    queued = true;
-    window.setTimeout(function() {
+    var queued = false;
+    var $lazyImages;
+    var $document = $(document);
+    var $window = $(window);
 
-        // Get nodes to animate
-        if (!$lazyImages) {
-            $lazyImages = $('img.lazy');
+    var loadImgSrc = function($img) {
+        if ($img.data('srcset')) {
+            $img.attr('srcset', $img.data('srcset'));
+        } else if ($img.data('src')) {
+            $img.attr('src', $img.data('src'));
+        }
+    };
+
+    var checkForLazyImages = function(delay) {
+
+        if (isNaN(delay)) {
+            delay = 50;
         }
 
-        // Remove lazy class if scroll position is reached
-        var scrollBottom = $document.scrollTop() + ($window.height() * (1 + OFFSET_PERCENT));
-        $lazyImages.each(function() {
-            var $lazyImage = $(this);
-            if (scrollBottom > $lazyImage.offset().top) {
-                loadImgSrc($lazyImage);
-                $lazyImage.removeClass('lazy');
+        // Avoid continuous scroll events
+        if (queued) {
+            return;
+        }
+        queued = true;
+        setTimeout(function() {
+
+            // Get nodes to animate
+            if (!$lazyImages) {
+                $lazyImages = $('img.lazy');
             }
-        });
 
+            // Remove lazy class if scroll position is reached
+            var scrollBottom = $document.scrollTop() + ($window.height() * (1 + OFFSET_PERCENT));
+            $lazyImages.each(function() {
+                var $lazyImage = $(this);
+                if (scrollBottom > $lazyImage.offset().top) {
+                    loadImgSrc($lazyImage);
+                    $lazyImage.removeClass('lazy');
+                }
+            });
+
+            $lazyImages = $('img.lazy');
+
+            // Remove scroll listener when there is no more nodes to animate
+            if ($lazyImages.length === 0) {
+                $window.off('scroll', checkForLazyImages);
+                $window.off('resize', checkForLazyImages);
+            }
+            queued = false;
+        }, delay);
+    };
+
+    $window.on('scroll', checkForLazyImages);
+    $window.on('resize', checkForLazyImages);
+    $(function() {
+        checkForLazyImages(0);
+    });
+
+    /**
+     * Refresh list of lazy loaded images. Used if images are placed in dom async with ajax.
+     * @return        {void}
+     */
+    exports.refresh = function() {
         $lazyImages = $('img.lazy');
+        checkForLazyImages(0);
+    };
 
-        // Remove scroll listener when there is no more nodes to animate
-        if ($lazyImages.length === 0) {
-            $window.off('scroll', checkForLazyImages);
-            $window.off('resize', checkForLazyImages);
-        }
-        queued = false;
-    }, 50);
-};
-
-$window.on('scroll', checkForLazyImages);
-$window.on('resize', checkForLazyImages);
-$(checkForLazyImages);
-
-/**
- * Refresh list of lazy loaded images. Used if images are placed in dom async with ajax.
- * @return        {void}
- */
-module.exports.refresh = function() {
-    $lazyImages = $('img.lazy');
-    checkForLazyImages();
-};
+    return exports;
+}));
---
+++ test/utils/lazy-images.js
@@ -0,0 +1,35 @@
+describe('ds.utils.lazyImages', function() {
+
+    it('should load images async with data-src', function(done) {
+        var $img = $('<img data-src="test/lazy-images-tester.jpg" class="lazy" style="height:1px;width:1;margin:0;padding:0;position:absolute;">');
+
+        $img.on('load', function() {
+            expect($img.attr('src')).to.equal('test/lazy-images-tester.jpg');
+            $img.remove();
+            done();
+        });
+        $img.prependTo('body');
+        expect($img.attr('src')).to.be.undefined;
+        ds.utils.lazyImages.refresh();
+    });
+
+    it('should load images async with data-srcset', function(done) {
+        var $img = $('<img data-srcset="test/lazy-images-tester.jpg" class="lazy" style="height:1px;width:1;margin:0;padding:0;position:absolute;">');
+        $img.prependTo('body');
+        expect($img.attr('src')).to.be.undefined;
+        var checkSrcSet = function() {
+            setTimeout(function() {
+                if ($img.attr('srcset')) {
+                    expect($img.attr('srcset')).to.equal('test/lazy-images-tester.jpg');
+                    $img.remove();
+                    done();
+                } else {
+                    checkSrcSet();
+                }
+            }, 10);
+        };
+        checkSrcSet();
+        ds.utils.lazyImages.refresh();
+    });
+
+});
